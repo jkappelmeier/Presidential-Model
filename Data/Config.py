25c2092d4bc5975d0ea99c38d5dc01199b87b239
John Kappelmeier <jak485@cornell.edu>
2024-08-24 20:36:27 -0400

    Added State Fundamentals

--- Data/Config.py
+++ Data/Config.py
@@ -10,6 +10,11 @@ incParty = 'D' # Incumbent Party
 incCandidate = 'Kamala Harris'
 chalCandidate = 'Donald Trump'
 
+incPresHomeState = 'California'
+chalPresHomeState = 'Florida'
+incVPHomeState = 'Minnesota'
+chalVPHomeState = 'Ohio'
+
 currentDate = datetime.date.today() # Current Date
 electionDate = datetime.date(2024,11,5) # Election Date
 startDate =  datetime.date(2024,7,22) # Campaign Start Date
@@ -26,3 +31,10 @@ incSigma = 0.0501 # Standard deviation in incumbent 2-party vote share
 pollingSigmaSF = 0.04 # Average polling error at N = 1000
 pollingProcessNoiseNat = 7.15e-6 # Polling process noise per day for national polls
 pollingBiasSigmaNat = np.sqrt(0.000617) # National Polling Bias Noise
+
+
+### State-Level Fundamental Constants
+presHomeStateBoost = 0.1055 # This is not a percentage but rather in "normalized format"
+# To convert from a normalized value to percent -> 1 / (1 + exp(-1*x)). Note that this value
+# is a CHANGE in the normalized value not the normalized value itself.
+vpHomeStateBoost = 0.0302
--- Data/LoadData.py
+++ Data/LoadData.py
@@ -1,8 +1,36 @@
 import numpy as np
 import csv
 import sys
+import Config as C
 sys.path.insert(1, '../Model')
 import Poll
+import Geography
+
+# Load in State Data
+states = []
+with open('../Data/StateData.csv') as csvfile:
+    data = csv.reader(csvfile, delimiter = ',')
+    rowCount = 0
+    for row in data:
+        if rowCount > 0:
+            if C.incParty == 'D':
+                est = float(row[1])
+            elif C.incParty == 'R':
+                est = 1 - float(row[1])
+
+            # Adjust for home state advantages
+            if str(row[0]) == C.incPresHomeState:
+                est = 1 / (1 + np.exp(-1 * (np.log(est / (1 - est)) + C.presHomeStateBoost)))
+            if str(row[0]) == C.chalPresHomeState:
+                est = 1 / (1 + np.exp(-1 * (np.log(est / (1 - est)) - C.presHomeStateBoost)))
+            if str(row[0]) == C.incVPHomeState:
+                est = 1 / (1 + np.exp(-1 * (np.log(est / (1 - est)) + C.vpHomeStateBoost)))
+            if str(row[0]) == C.chalVPHomeState:
+                est = 1 / (1 + np.exp(-1 * (np.log(est / (1 - est)) - C.vpHomeStateBoost)))
+
+            state = Geography.Geography(str(row[0]), est, float(row[2]), float(row[3]))
+            states.append(state)
+        rowCount = rowCount + 1
 
 # Load in National Polls
 natPollsData = []
